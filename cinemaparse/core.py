50d1b48510006ced7c44d7a9d47f9b145215d691
andreyka7T <45653472+andreyka7T@users.noreply.github.com>
2019-11-10 14:25:44 +0300

    Update core.py

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -11,13 +11,12 @@ class CinemaParser:
 
     def extract_raw_content(self):
         """Cinema content"""
-        main_page_url = "https://msk.subscity.ru"
+        main_page_url = "https://"+self.city+".subscity.ru"
         get_main_page_response = requests.get(main_page_url)
         self.content = BeautifulSoup(get_main_page_response.text, 'html.parser')
 
     def print_raw_content(self):
         """content printer"""
         print(self.content.prettify())
-
 CINEMA_ABOUT_MOSCOW = CinemaParser('msk')
 CINEMA_ABOUT_MOSCOW.extract_raw_content()
